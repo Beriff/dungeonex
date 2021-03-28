ed9178ab6d2012e9b616b49c540911e5b794d8b7
Beriff <dofite.yes@gmail.com>
2021-03-28 21:00:56 +0700

    Add support for 2 active tabs

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import discord
 import game
 from discord.ext import commands
+from copy import deepcopy
 
 client = commands.Bot(command_prefix = '.')
 
 control_messages = {}
+
+enemies = {"monkey": game.enemy}
 
 def get_game_embed(user) -> discord.Embed:
     embed = discord.Embed(title='Dungeon Name', description=game.game_instances[user.name].get_printable(), color=0xffffff)
@@ -23,14 +26,21 @@ async def end_game(ctx, user) -> None:
     print(f'[!] {user.name} game ended')
     print(f"[>] {len(game.game_instances)} Game Instances are active")
 
-    await ctx.send('Thanks for playing!')
+    await ctx.send('Game over!')
+
+async def show_inv(ctx, user) -> None:
+    game.game_instances[user.name].focused_window = int(not bool(game.game_instances[user.name].focused_window))
+    await ctx.message.edit(embed=get_game_embed(user))
 
 @client.event
 async def on_ready():
+    await client.change_presence(activity=discord.Game(name=".info"))
     print('[!] Dungeons are ready')
 
 @client.command()
 async def newgame(ctx):
+    if ctx.author.name in game.game_instances:
+        return False
     newgame = game.Game(9, 8)
     newgame._generate_basic_grid()
     newgame.entities.append(game.rat_hero)
@@ -45,9 +55,17 @@ async def newgame(ctx):
 
     control_messages[ctx.author.name] = message
     await control_messages[ctx.author.name].add_reaction('⬆')
+    await control_messages[ctx.author.name].add_reaction('⬅')    
     await control_messages[ctx.author.name].add_reaction('⬇')
-    await control_messages[ctx.author.name].add_reaction('⬅')
     await control_messages[ctx.author.name].add_reaction('➡')
+    await control_messages[ctx.author.name].add_reaction('⏺')
+
+@client.command()
+async def control(ctx, username, command, param_a):
+    active = game.game_instances[username]
+    if command == "add":
+        active.entities.append(deepcopy(enemies[param_a]))
+        await ctx.send('Executed')
 
 @client.event
 async def on_reaction_add(reaction, user):
@@ -70,9 +88,16 @@ async def on_reaction_add(reaction, user):
                     ctx = await client.get_context(reaction.message)
                     await moveright(ctx, user)
                     await reaction.message.remove_reaction('➡', user)
+                elif reaction.emoji == '⏺':
+                    ctx = await client.get_context(reaction.message)
+                    await show_inv(ctx, user)
+                    await reaction.message.remove_reaction('⏺', user)
+
 
         except KeyError:
             print(f"[!] {user.name} tried to access foreign game")
+        except:
+            pass
 
 @client.command()
 async def endgame(ctx):
@@ -111,4 +136,4 @@ async def moveright(ctx, user):
         else:
             await ctx.message.edit(embed=get_game_embed(user))
 
-client.run('ODI1MjAyNTgxNjI1NTAzNzU0.YF6fyg.p23MV1Bong_T9V3f7AzpaFsL_4Y')
+client.run('TOKEN')
